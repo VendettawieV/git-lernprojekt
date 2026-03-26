2339ee607df03e34fdba74dbdc58948748c228e2
Praktikant <praktikant@dsv.de>
2026-03-26 08:46:22 +0100

    Rechner: Addition, Subtraktion und Division

---
+++ rechner.py
@@ -0,0 +1,15 @@
+# Einfacher Rechner – Übung 04
+
+def addiere(a, b):
+    return a + b
+
+def subtrahiere(a, b):
+    return a - b
+
+def dividiere(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("10 + 5 =", addiere(10, 5))
+    print("10 - 5 =", subtrahiere(10, 5))
+    print("10 / 5 =", dividiere(10, 5))
