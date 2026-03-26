37f1a4be72b07f195670e8d663aab93e64f8a8ad
Praktikant <praktikant@dsv.de>
2026-03-26 08:46:16 +0100

    Basis-Rechner für Übung 02 bereitgestellt

---
+++ rechner.py
@@ -0,0 +1,18 @@
+# Einfacher Rechner – Übung 02
+
+def addiere(a, b):
+    return a + b
+
+def subtrahiere(a, b):
+    return a - b
+
+def multipliziere(a, b):
+    return a * b
+
+# TODO: Füge hier eine begruessung(uhrzeit) Funktion hinzu!
+# Lies die genaue Aufgabe in: aufgaben/uebung-02.md
+
+if __name__ == "__main__":
+    print("5 + 3 =", addiere(5, 3))
+    print("9 - 4 =", subtrahiere(9, 4))
+    print("6 * 7 =", multipliziere(6, 7))
